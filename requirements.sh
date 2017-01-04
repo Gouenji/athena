be9b5a7e205882253c1a3a9cb95f6ecdc50963e1
Mehul Kumar <Gouenji@users.noreply.github.com>
2017-01-04 18:42:55 +0530

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,34 @@
+sudo apt-get install python-pip python-dev build-essential 
+sudo apt-get install python3-pip python3-dev build-essential
+sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
+sudo pip install --upgrade virtualenv 
+sudo pip install pygsr
+sudo pip install pygst
+sudo pip install pyttsx
+sudo pip install SpeechRecognition
+sudo pip3 install google_speech
+sudo apt-get install libttspico-utils
+sudo pip install httpclient
+sudo apt-get install sox libsox-fmt-mp3
+sudo pip install -r a.txt
+sudo apt-get install python3-matplotlib
+sudo pip install gTTS
+sudo apt-get install espeak
+sudo pip install speech
+sudo apt-get install python-pyaudio python3-pyaudio
+sudo apt-get install libasound-dev
+git clone http://people.csail.mit.edu/hubert/git/pyaudio.git
+cd pyaudio
+sudo python setup.py install
+sudo pip install microphone
+sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo apt-get install python-pyaudio
+sudo apt-get install mpg321
+sudo apt-get install sphinxsearch
+sudo add-apt-repository ppa:dhuggins/cmusphinx
+sudo apt-get update
+sudo apt-get python-xlib python-simplejson python-gtk2 python-gst0.1 \
+gstreamer0.10-pocketsphinx sphinx-common python-sphinxbase \
+python-pocketsphinx sphinxbase-utils git
